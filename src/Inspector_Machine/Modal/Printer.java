df93badc13d4ce3b265dafab9376f6ae07c82ecb
Ruwan <ruwan0601>
2018-07-13 07:46:07 +0530

    InspectorMachine.Modal classes are done

---
+++ src/Inspector_Machine/Modal/Printer.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Inspector_Machine.Modal;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author hp
+ */
+public class Printer implements Serializable {
+
+    int printerId;
+
+    void printReciept() {
+
+    }
+}
